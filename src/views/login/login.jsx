d69914a4e96fc382877fc79fcf019d6809acdc40
Andrew <andydrew313@gmail.com>
2019-08-06 13:35:48 -0700

    http

--- src/views/login/login.jsx
+++ src/views/login/login.jsx
@@ -25,7 +25,7 @@ const Login = (props) => {
 
   const checkCredentials = (email, pass) => {
     console.log("test", process.env.REACT_APP_SERVER + "/data/users/login")
-    Axios.defaults.baseURL="/";
+    Axios.defaults.baseURL="http://";
     Axios.post(process.env.REACT_APP_SERVER + "/data/users/login", {
       email: email,
       password: pass
