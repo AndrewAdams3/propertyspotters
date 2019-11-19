28c2de6346db94f4f286efef81d115010ebb0685
Andrew Adams <andyd@swuser-129-8-218-193.swlan.csufresno.edu>
2019-11-19 14:31:24 -0800

    api back

--- src/components/map/MyMap.jsx
+++ src/components/map/MyMap.jsx
@@ -75,7 +75,7 @@ const MyMap = ({markers, tracks}) => {
     return(
         <>
         <MapBox
-         // apiKey={process.env.REACT_APP_GOOGLE_API_KEY}
+          apiKey={process.env.REACT_APP_GOOGLE_API_KEY}
           opts={{
             center: {lat: 36.737797, lng: -119.787125},
             zoom: 6,
@@ -101,7 +101,7 @@ const MyMap = ({markers, tracks}) => {
                 path:track.line,
               }}
               onMouseOver={(e)=>setClick({e, track})}
-              onMouseOut={()=>setClick()}
+              onMouseOut={()=>setTimeout(setClick, 200)}
             />
           ))
         }
